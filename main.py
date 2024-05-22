62806aab02bd2f17d3b100ee0db41796a328db73
VisionExpo <gorulevishal984@gmail.com>
2024-05-22 13:20:06 +0530

    changes in main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+#from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 #from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 #from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
@@ -21,7 +21,7 @@ except Exception as e:
 
 
 
-STAGE_NAME = "Prepare base model"
+'''STAGE_NAME = "Prepare base model"
 try: 
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -30,11 +30,11 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
-        raise e
+        raise e'''
 
 
 
-STAGE_NAME = "Training"
+'''STAGE_NAME = "Training"
 try: 
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -43,7 +43,7 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
-        raise e
+        raise e'''
 
 
 
